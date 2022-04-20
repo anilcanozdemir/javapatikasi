533c770ab5ef1adfdf35561bdc6773119a888254
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2022-04-20 13:25:24 +0300

    javadoc

--- src/Giris/Baslangic.java
+++ src/Giris/Baslangic.java
@@ -1,7 +1,11 @@
 package Giris;
 
+/**
+ * @author Anıl Can Özdemir
+ */
 public class Baslangic {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
     }
 }
